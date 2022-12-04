bbd89222cc18f211cbe3c6f5f6d59542d9525ef0
azerc <zizimoos@likelion.org>
2022-12-04 16:55:47 +0900

    deploy cloudtype

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 import cors from "cors";
 import ip from "ip";
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 8050;
 
 const app = express();
 app.use(cors());
